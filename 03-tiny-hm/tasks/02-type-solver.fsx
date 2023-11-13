59c0a14ae5c98d9ee268d2aaa88175a21bb053f7
BohdanQQ <40754203+BohdanQQ@users.noreply.github.com>
2023-11-13 15:46:32 +0100

    implement task 2

--- 03-tiny-hm/tasks/02-type-solver.fsx
+++ 03-tiny-hm/tasks/02-type-solver.fsx
@@ -11,26 +11,44 @@ type Type =
   | TyNumber 
   | TyList of Type
 
-let occursCheck vcheck ty =
-  // TODO: Return true of type 'ty' contains variable 'vcheck'
-  failwith "not implemented"
+let rec occursCheck vcheck ty =
+  // return true if type 'ty' contains variable 'vcheck'
+  match ty with
+    | TyVariable v -> v = vcheck
+    | TyBool | TyNumber -> false
+    | TyList t -> occursCheck vcheck t
  
-let rec substType (subst:Map<string, Type>) ty = 
-  // TODO: Apply all the specified substitutions to the type 'ty'
+let rec substType (subst:Map<string, Type>) ty  = 
+  // Apply all the specified substitutions to the type 'ty'
   // (that is, replace all occurrences of 'v' in 'ty' with 'subst.[v]')
-  failwith "not implemented"
+  let rec substTypeImpl ty v = 
+    match ty with
+      | TyVariable v1 -> if v = v1 then subst.[v] else ty
+      | TyBool | TyNumber -> ty
+      | TyList t -> TyList (substTypeImpl t v)
+  and f = (fun acc key vl -> substTypeImpl acc key)
+    in
+  Map.fold f ty subst
 
-let substConstrs (subst:Map<string, Type>) (cs:list<Type * Type>) = 
-  // TODO: Apply substitution 'subst' to all types in constraints 'cs'
-  failwith "not implemented"
- 
+
+let rec substConstrs (subst:Map<string, Type>) (cs:list<Type * Type>) = 
+  // Apply substitution 'subst' to all types in constraints 'cs'
+  cs |> List.map (fun (ty1, ty2) -> (substType subst ty1, substType subst ty2))
 
 let rec solve cs =
   match cs with 
   | [] -> []
-  | (TyNumber, TyNumber)::cs -> solve cs
-  // TODO: Fill in the remaining cases! You can closely follow the
-  // example from task 1 - the logic here is exactly the same.
+  | (TyNumber, TyNumber)::cs | (TyBool, TyBool)::cs -> 
+      solve cs
+  | (TyList t1, TyList t2)::cs -> 
+      solve ((t1, t2)::cs)
+  | (TyVariable v, t)::cs | (t, TyVariable v)::cs ->
+      if occursCheck v t then failwith "Cannot be solved (occurs check)"
+      let cs = substConstrs (Map.empty.Add(v, t)) cs
+      let subst = solve cs
+      let t = substType (Map.ofList subst) t
+      (v, t)::subst
+   | _ -> failwith "Cannot be solved"
 
 
 // ----------------------------------------------------------------------------
